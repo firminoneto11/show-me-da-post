d9d0a1f2549e63c9ac344c02d75541fc955562e9
Firmino Neto <firminoneto11@hotmail.com>
2021-08-23 20:26:49 -0300

    Usability corrections

--- posts/static/js/scroll.js
+++ posts/static/js/scroll.js
@@ -1,8 +1,7 @@
-
+const back_to_top_button = document.getElementById('btp')
 
 window.onscroll = () => {
-    const back_to_top_button = document.getElementById('btp')
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         back_to_top_button.style.display = "block";
     } else {
         back_to_top_button.style.display = "none";
--- users/static/js/popup.js
+++ users/static/js/popup.js
@@ -1,11 +1,21 @@
 
 // Function that will manage the behavior of the popup close button
 const manage_popup = () => {
-    const popup = document.getElementById('popup')
-    popup.classList.remove('show')
-    popup.classList.add('hide')
+    try {
+        const popup = document.getElementById('popup')
+        popup.classList.remove('show')
+        popup.classList.add('hide')
+    }
+    catch {
+        //
+    }
 }
 
 // Binding the event listeners
 document.addEventListener('DOMContentLoaded', () => setTimeout(manage_popup, 5000))
-document.getElementById('close_button').addEventListener('click', manage_popup)
+try {
+    document.getElementById('close_button').addEventListener('click', manage_popup)
+}
+catch {
+    //
+}
