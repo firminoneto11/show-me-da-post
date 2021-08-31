a324f658aa844bdba5a856c20bfca13b65b5960b
Firmino Neto <firminoneto11@hotmail.com>
2021-08-31 20:05:12 -0300

    Fix on some interface aspects

--- posts/static/js/modal.js
+++ posts/static/js/modal.js
@@ -3,7 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const new_post = document.getElementById("new_post")
     const modal = document.getElementById("modal")
     const close_button = document.getElementById("close_button")
-    new_post.addEventListener("click", () => { modal.classList.add("modal-active") })
+    new_post.addEventListener("click", () => {
+        modal.classList.add("modal-active")
+        setTimeout(() => { document.getElementById("title").focus() }, 0500)
+    })
     close_button.addEventListener("click", () => { modal.classList.remove("modal-active") })
 })
 
