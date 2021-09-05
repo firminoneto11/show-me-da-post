805eca709f262973016f6f5679d1ff2bd37da39e
Firmino Neto <firminoneto11@hotmail.com>
2021-09-05 19:18:58 -0300

    Configuration on the wsgi file

--- A_show_me_da_post/wsgi.py
+++ A_show_me_da_post/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
-from django.conf.global_settings import DEBUG
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A_show_me_da_post.settings')
 
-development_app = Cling(MediaCling(get_wsgi_application()))
-production_app = get_wsgi_application()
-
-application = development_app if DEBUG else production_app
+if settings.DEBUG:
+    application = get_wsgi_application()
+else:
+    application = Cling(MediaCling(get_wsgi_application()))
